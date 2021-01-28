3fa2dab118b74d2be1680bae0faa14cfd98997c8
Luke Oxley <lukeoxley9@gmail.com>
2021-01-28 15:34:47 -0500

    Added untested DAC support

--- CommandParser.ino
+++ CommandParser.ino
@@ -7,12 +7,22 @@ const int NUM_MCP_PINS = 4;
 
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   /*
   // Try to initialize! : 
   // https://github.com/hideakitai/MCP4728/blob/master/MCP4728.h
    */
+  Wire.begin();
+  mcp.attach(Wire, 14); // 14 is the enable pin
+  mcp.readRegisters();
+  mcp.analogWrite(0,0);
+  mcp.analogWrite(1,0);
+  mcp.analogWrite(2,0);
+  mcp.analogWrite(3,0);
+  mcp.enable(true);
+  mcp.readRegisters();
+  delay(100);
 }
 /* Message types: 
     READ 
@@ -63,6 +73,14 @@ void serialEvent() {
     }
     else if(message.indexOf("DAC") != -1) {
       //DAC type
+      removeWord(&message);
+      int pin = message.substring(0, message.indexOf(' ')).toInt(); 
+      int value = message.substring(message.indexOf(' ')).toInt();
+      if(pin < NUM_MCP_PINS) mcp.analogWrite(pin, value);
+      else commandError(3);
+      delay(10);
+      mcp.readRegisters();
+
     }
     else{
       commandError(2);
